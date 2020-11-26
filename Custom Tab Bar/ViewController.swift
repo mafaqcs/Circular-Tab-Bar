15f1af187853cfd9eaf78ce7df6dfabeeee49344
Muhammad Afaq <mafaqcs@gmail.com>
2020-11-27 01:12:02 +0500

    Initial Commit

---
+++ Custom Tab Bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Tab Bar
+//
+//  Created by M Afaq on 27/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
